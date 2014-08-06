89b2be88780f09bdd9f8236c525ef69e0ba7be73
vicmars5 <mars.vic5@gmail.com>
2014-08-06 13:19:25 -0600

    Iniciando
    Apenas creado un nav

---
+++ Admin/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Administración Pagina</title>
+</head>
+<body>
+	<nav>
+		<ul>
+			<li>Nuevo Post</li>
+			<li>Posts</li>
+			<li>Nuevo Usuario</li>
+			<li>Usuarios</li>
+		</ul>
+		<?php 
+		session_start();
+		 ?>
+	</nav>
+</body>
+</html>
